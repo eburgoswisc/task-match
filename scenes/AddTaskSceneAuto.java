b1696ad06798381c9d4bf2e56e1922f151d30f35
Ritik <rtkg12@gmail.com>
2019-04-22 21:40:55 -0500

    Partially implemented AddTaskSceneAuto

--- scenes/AddTaskSceneAuto.java
+++ scenes/AddTaskSceneAuto.java
@@ -4,15 +4,62 @@
 
 package scenes;
 
-import javafx.scene.Parent;
+import java.io.File;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
 public class AddTaskSceneAuto extends Scene {
   Stage mainStage;
-  
-  public AddTaskSceneAuto(Stage mainStage, Parent root, double width, double height) {
+
+  public AddTaskSceneAuto(Stage mainStage, BorderPane root, double width, double height) {
     super(root, width, height);
     this.mainStage = mainStage;
+
+    root.setPadding(new Insets(20));
+
+    // Top Label
+    Label addTaskTop = new Label("Add Tasks");
+    root.setTop(addTaskTop);
+    BorderPane.setAlignment(addTaskTop, Pos.TOP_CENTER);
+
+    // Input field Center
+    HBox inputs = new HBox(10);
+    inputs.setPadding(new Insets(20, 20, 20, 20));
+
+    FileChooser fileChooser = new FileChooser();
+    Button fileChooseButton = new Button("Input File");
+
+    fileChooser.setTitle("Input File");
+
+    fileChooser.getExtensionFilters().addAll(new ExtensionFilter("JSON Files", "*.json"));
+    fileChooseButton.setOnAction(e -> {
+      File selectedFile = fileChooser.showOpenDialog(mainStage);
+    });
+
+    Label fileName = new Label("input.json"); // TODO change to actual filename
+
+    // if (selectedFile != null) {
+    // mainStage.display(selectedFile);
+    // }
+
+    inputs.getChildren().addAll(fileChooseButton, fileName);
+    inputs.setPadding(new Insets(200, 300, 100, 300));
+    root.setCenter(inputs);
+
+    // Bottom nav bar and buttons
+    HBox navBar = new HBox(10);
+    Button addNewTask = new Button("Add New Task");
+    Button back = new Button("Back");
+    navBar.getChildren().addAll(addNewTask, back);
+    root.setBottom(navBar);
+    // mainStage.show();
   }
 }
