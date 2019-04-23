7261336adc8e2962cc8c997e2c67ac4f4d8f6b03
Adam Jackson <amjackson6@wisc.edu>
2019-04-22 19:46:47 -0500

    added class files for different scenes

--- application/Main.java
+++ application/Main.java
@@ -1,26 +1,31 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
+import scenes.HomeScene;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+  
+  Scene homeScene;
+  Scene addEmployee;
+  
+  @Override
+  public void start(Stage primaryStage) {
+    try {
+      BorderPane root = new BorderPane();
+      Scene homeScene = new HomeScene(root, 400, 400);
+      homeScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+      primaryStage.setScene(homeScene);
+      primaryStage.show();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
---
+++ scenes/HomeScene.java
@@ -0,0 +1,16 @@
+package scenes;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HomeScene extends Scene {
+
+  Stage root;
+  
+  public HomeScene(Parent root, int width, int height) {
+    super(root,width,height);
+    
+    
+  }  
+}
