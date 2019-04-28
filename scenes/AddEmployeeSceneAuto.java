4a241ec4949e1b798796e2b6849a17369037b4cb
Ritik <rtkg12@gmail.com>
2019-04-28 17:56:14 -0500

    Add file chooser to add employees auto

--- scenes/AddEmployeeSceneAuto.java
+++ scenes/AddEmployeeSceneAuto.java
@@ -3,6 +3,7 @@
  */
 package scenes;
 
+import java.io.File;
 import application.Main;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -14,63 +15,72 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 public class AddEmployeeSceneAuto extends Scene {
-  
+
   Stage mainStage;
-  
+
   public AddEmployeeSceneAuto(Stage mainStage, BorderPane root, double width, double height) {
     super(root, width, height);
     this.mainStage = mainStage;
-    
-    
-	root.autosize();
+
+
+    root.autosize();
     root.setPadding(new Insets(10));
-    
+
     Button inputFile = new Button("Input file");
-    
+
+    FileChooser fileChooser = new FileChooser();
+
+    fileChooser.setTitle("Input File");
+
+    fileChooser.getExtensionFilters().addAll(new ExtensionFilter("JSON Files", "*.json"));
+    inputFile.setOnAction(e -> {
+      File selectedFile = fileChooser.showOpenDialog(mainStage);
+    });
+
     Button addToUnit = new Button("Add to Unit");
-    
+
     Button addNewEmployee = new Button("Add New Employee");
     addNewEmployee.setOnAction(e -> {
       Main.switchToAddEmployeesManual(this.mainStage);
-      });
-    
+    });
+
     Button back = new Button("Back");
     back.setOnAction(e -> {
       Main.switchToHome(this.mainStage);
-      });
-    
-    ObservableList<String> employees = FXCollections.observableArrayList(
-			"employee1", "employee2", "employee3", "employee4", "employee5");
-	ComboBox comboBox = new ComboBox(employees); //create a combo box object
-	Label l = new Label("                                                  "
-			+ "                                 "
-			+ "        ");
-    
+    });
+
+    ObservableList<String> employees = FXCollections.observableArrayList("employee1", "employee2",
+        "employee3", "employee4", "employee5");
+    ComboBox comboBox = new ComboBox(employees); // create a combo box object
+    Label l = new Label("                                                  "
+        + "                                 " + "        ");
+
     HBox line1 = new HBox(10);
     HBox line2 = new HBox(10);
     HBox line3 = new HBox(10);
     HBox empty = new HBox(50);
-    
-    
+
+
     line1.getChildren().addAll(inputFile, new Label("data.json"));
-    
+
     line2.getChildren().addAll(comboBox, addToUnit);
-    
+
     line3.getChildren().addAll(addNewEmployee, empty, l, back);
     empty.getChildren().addAll(l);
-    
-    
+
+
     line1.setPrefSize(500, 250);
     root.setTop(line1);
     root.setCenter(line2);
     root.setBottom(line3);
-    
-    
-    
-    
+
+
+
   }
 
 }
