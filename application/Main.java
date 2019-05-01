a903be1d395987ff873c5ac7f2132cbd8f3c104b
Adam Jackson <amjackson6@wisc.edu>
2019-05-01 10:26:32 -0500

    Added alert popup to confirm save when program terminates

--- application/Main.java
+++ application/Main.java
@@ -3,6 +3,9 @@ package application;
 import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -14,7 +17,7 @@ public class Main extends Application {
   private static ArrayList<Task> allTasks;
   private static String curFileOpenPath;
   private static String curFileOpenName;
-
+  
   @Override
   public void start(Stage primaryStage) {
     curFileOpenPath = "default.json";
@@ -22,9 +25,22 @@ public class Main extends Application {
     allTasks = new ArrayList<>();
     allEmployees = new ArrayList<>();
     employeesInUnit = new ArrayList<>();
+    
     primaryStage.setOnCloseRequest(e -> {
-      JSONFileParser.write(curFileOpenPath);
+      if(!curFileOpenName.equals("file not found")) {
+        Alert closeAlert = new Alert(AlertType.CONFIRMATION, "Would you like to save \'"+curFileOpenName+"\'? (Click Ok to save, "
+            + "Cancel to exit without saving");
+        closeAlert.showAndWait();
+        if(closeAlert.getResult().equals(ButtonType.OK)) {
+          JSONFileParser.write(curFileOpenPath);
+        }
+      }
+      else {
+        Alert closeAlert = new Alert(AlertType.INFORMATION, "No file open to save, press OK to terminate the program.");
+        closeAlert.showAndWait();
+      }
     });
+    
     try {
       BorderPane root = new BorderPane();
       currentScene = new HomeScene(primaryStage, root, 920, 600);
